1bf5c2a70f27a74b57b186c6e4c6e8b4190f1fa4
jdlynch13 <jimmyl@brimmatech.com>
2023-05-05 12:14:14 -0400

    completed most of the interface and css, beginning work on scripts

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,3 @@
-// If you would like to see some examples of similar code to make an interface interact with an API, 
-// check out the coin-server example from a previous COMP 426 semester.
-// https://github.com/jdmar3/coinserver
+const gameType = document.getElementsByName("gameType");
+const gamemMode = document.getElementsByName("gameMode");
+const move = document.getElementById("move");
