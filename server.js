ce3d6d2144c55bbf44c2d3903d5d678f286bc06e
jdlynch13 <jimmyl@brimmatech.com>
2023-05-05 12:22:17 -0400

    updated server.js by adding API Endpoints from A04

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+
+import {rps, rpsls} from './lib/rpsls.js';
+
 //// Load most basic dependencies
 // Create require function 
 // https://nodejs.org/docs/latest-v18.x/api/module.html#modulecreaterequirefilename
@@ -101,4 +104,59 @@ process.on('SIGINT', () => {
             console.info('\n' + stoppedlog)
         }    
     })
+})
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+// Check endpoint @ /app
+app.get('/app', (req, res) => {
+    res.status(200).send("200 OK");
+})
+
+// check endpoint at /app/rps
+app.get('/app/rps/', (req, res) => {
+    res.status(200).send(JSON.stringify(rps()));
+})
+
+// /app/rpsls endpoint
+app.get('/app/rpsls/', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls()));
+})
+
+// /app/rps/play endpoint (bodies & params)
+app.get('/app/rps/play/', (req, res) => { //rps URLEncoded
+    res.status(200).send(JSON.stringify(rps(req.query.shot)));
+})
+
+app.post('/app/rps/play/', (req, res) => { //rps JSON
+    res.status(200).send(JSON.stringify(rps(req.body.shot)));
+})
+
+app.get('/app/rps/play/:shot', (req, res) => { //rps shot params
+    res.status(200).send(JSON.stringify(rps(req.params.shot)));
+})
+
+// /app/rpsls/play endpoint
+app.get('/app/rpsls/play/', (req, res) => { //rpsls URLEncoded
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
+})
+
+app.post('/app/rpsls/play/', (req, res) => { //rpsls JSON
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
+})
+
+app.get('/app/rpsls/play/:shot', (req, res) => { //rpsls shot params
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
+})
+
+
+// default endpoint)
+app.get("/app/*", (req, res) => {
+    res.status(404).send("404 NOT FOUND");
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
 })
