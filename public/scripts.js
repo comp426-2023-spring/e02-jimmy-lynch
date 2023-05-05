fcf773a106808c5126662351aa857860033cdd73
jdlynch13 <jimmyl@brimmatech.com>
2023-05-05 12:44:28 -0400

    playing around with scripts

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,43 @@
-const gameType = document.getElementsByName("gameType");
-const gamemMode = document.getElementsByName("gameMode");
-const move = document.getElementById("move");
+const rps_moves = ["rock", "paper", "scissors"];
+const rpsls_moves = ["rock", "paper", "scissors", "lizard", "spock"];
+
+function showMoves() {
+    move.innerHTML = "";
+    if (document.getElementById('rps').checked) {
+        rps_moves.forEach(m => {
+            const x = document.createElement("move");
+            x.value = m;
+            x.textConent = x;
+            move.appendChild(x);
+        });
+        console.log("TEST")
+    } else if (document.getElementById('rpsls').checked) {
+        rpsls_moves.forEach(m => {
+            const x = document.createElement("move");
+            x.value = m;
+            x.textConent = x;
+            move.appendChild(x);
+        });
+        console.log("TEST2");
+    }
+}
+
+const playButton = document.getElementById("play");
+playButton.addEventListener
+
+async function play() {
+    console.log("TEST2");
+    showMoves();
+}
+
+playButton.addEventListener("click", () => {
+    play();
+  });
+
+function reset() {
+
+}
+
+function toggleResults() {
+
+}
--- server.js
+++ server.js
@@ -73,39 +73,7 @@ app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:htt
 ))
 // Serve static files
 const staticpath = args.stat || args.s || process.env.STATICPATH || path.join(__dirname, 'public')
-app.use('/', express.static(staticpath))
-// Create app listener
-const server = app.listen(port)
-// Create a log entry on start
-let startlog = new Date().toISOString() + ' HTTP server started on port ' + port + '\n'
-// Debug echo start log entry to STDOUT
-if (args.debug) {
-    console.info(startlog)
-} 
-// Log server start to file
-fs.appendFileSync(path.join(logpath, 'server.log'), startlog)
-// Exit gracefully and log
-process.on('SIGINT', () => {
-// Create a log entry on SIGINT
-    let stoppinglog =  new Date().toISOString() + ' SIGINT signal received: stopping HTTP server\n'
-//  Log SIGINT to file
-    fs.appendFileSync(path.join(logpath, 'server.log'), stoppinglog)
-// Debug echo SIGINT log entry to STDOUT
-    if (args.debug) {
-        console.info('\n' + stoppinglog)
-    }
-// Create a log entry on stop
-    server.close(() => {
-        let stoppedlog = new Date().toISOString() + ' HTTP server stopped\n'
-// Log server stop to file
-        fs.appendFileSync(path.join(logpath, 'server.log'), stoppedlog)
-// Debug echo stop log entry to STDOUT
-        if (args.debug) {
-            console.info('\n' + stoppedlog)
-        }    
-    })
-})
-
+app.use('/', express.static(staticpath));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
@@ -160,3 +128,37 @@ app.get("/app/*", (req, res) => {
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 })
+
+// Create app listener
+const server = app.listen(port)
+// Create a log entry on start
+let startlog = new Date().toISOString() + ' HTTP server started on port ' + port + '\n'
+// Debug echo start log entry to STDOUT
+if (args.debug) {
+    console.info(startlog)
+} 
+// Log server start to file
+fs.appendFileSync(path.join(logpath, 'server.log'), startlog)
+// Exit gracefully and log
+process.on('SIGINT', () => {
+// Create a log entry on SIGINT
+    let stoppinglog =  new Date().toISOString() + ' SIGINT signal received: stopping HTTP server\n'
+//  Log SIGINT to file
+    fs.appendFileSync(path.join(logpath, 'server.log'), stoppinglog)
+// Debug echo SIGINT log entry to STDOUT
+    if (args.debug) {
+        console.info('\n' + stoppinglog)
+    }
+// Create a log entry on stop
+    server.close(() => {
+        let stoppedlog = new Date().toISOString() + ' HTTP server stopped\n'
+// Log server stop to file
+        fs.appendFileSync(path.join(logpath, 'server.log'), stoppedlog)
+// Debug echo stop log entry to STDOUT
+        if (args.debug) {
+            console.info('\n' + stoppedlog)
+        }    
+    })
+})
+
+
